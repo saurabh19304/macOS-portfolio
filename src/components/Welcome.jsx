784ff26f634882674eecbd54bac997d1ac2d8cc1
saurabh <saurabh20tiwari04@gmail.com>
2026-04-24 18:06:46 +0530

    implementing the welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -18,7 +18,7 @@ const renderText = (text, className, baseWeight = 400) => {
 )
 }
 const setupTextHover = (container, type) => {
-    if(!container) return;
+    if(!container) () => {};
 
     const letters = container.querySelectorAll('span')
     const { min, max, default: base } = FONT_WEIGHTS[type];
