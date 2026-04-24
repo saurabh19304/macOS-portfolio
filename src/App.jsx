c70c94839cfa2a9536550070d11a60a06bced4cb
saurabh <saurabh20tiwari04@gmail.com>
2026-04-24 22:41:29 +0530

    implementing the dock

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Navbar , Welcome } from './components'
+import { Navbar, Welcome, Dock } from './components'
 function App() {
     return (
         <main>
     <Navbar />
     <Welcome />
+    <Dock />
         </main>
     )
 }
