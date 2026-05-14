b6b733ab24ac725b03c9b98169f0c376a7684282
saurabh <saurabh20tiwari04@gmail.com>
2026-05-15 01:02:40 +0530

    implement hoc and terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import gsap from 'gsap';
 import { Navbar, Welcome, Dock } from './components'
+
+import { Draggable } from "gsap/Draggable";
+import {Terminal} from "#/windows/index.js";
+gsap.registerPlugin(Draggable);
+
 function App() {
     return (
         <main>
     <Navbar />
     <Welcome />
     <Dock />
+
+    <Terminal />
         </main>
     )
 }
